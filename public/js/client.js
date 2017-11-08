6f76b6e7f7fc6f73c3494468b3f1b39e93bfe094
turingtw <kevin83113081620@gmail.com>
2017-11-08 19:47:12 +0800

    first commit

--- public/js/client.js
+++ public/js/client.js
@@ -4,13 +4,6 @@ socket.on('example-pong', function (data) {
     console.log("pong");
 });
 
-window.addEventListener("load", function(){
-
-  var button = document.getElementById('hello');
-
-  button.addEventListener('click', function() {
-      console.log("ping");
-      socket.emit('example-ping', { duration: 2 });
-  });
-
-});
+socket.on('hit', function(data){
+	console.log(data);
+})
