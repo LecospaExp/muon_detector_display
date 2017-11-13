09c11b9a9fafbfa5e995efce0b5d6eb506c1038a
yuhsin <wang910654@gmail.com>
2017-11-13 20:33:16 +0800

    Change to chart.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,53 +1,31 @@
 var socket = io('http://140.112.104.83:9487');
 
-var channel_count = [0,0,0,0,0,0,0,0,0];
+var count = [76,43,11,100,40,11,50,23];
 
+var count_channel = new Chart('test', {
+	type: 'bar',
+    data: {
+    	labels: ["1","2","3","4","5","6","7","8","9"],
+    	datasets: [
+    		{
+    			labels: "Count",
+    			backgroundColor: "#3e95cd",
+    			data: count
+    		}
+    	]
+    },
+    options: {
+    	title: {
+    		display: true,
+    		text: "Count-Channel"
+    	}
+	}
+});
 
-(function(){
-	var d3 = Plotly.d3;
+socket.on('hit', function(channel_number){
+	console.log(channel_number);
+	channel_count[channel_number-1] += 1;
 
-	var WIDTH_IN_PERCENT_OF_PARENT = 60,
-	    HEIGHT_IN_PERCENT_OF_PARENT = 80;
+	count_channel.update();
 
-	var gd3 = d3.select('body')
-	    .append('div')
-	    .style({
-	        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
-	        'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
-
-	        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
-	        'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
-	    });
-
-	var gd = gd3.node();
-
-	var data_set = {
-		type: 'bar',
-		x: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-	    y: channel_count,
-	    marker:{
-	    	color: '#C8A2C8',
-	    	line: {width: 2.5}
-	    } 
-	};
-
-	var data = [data_set];
-
-	var layout = {
-		title: 'Count-Channel',
-		font: {size: 16}
-	};
-
-	Plotly.plot(gd, data, layout, {displayModeBar: false});
-
-	window.onresize = function() {
-	    Plotly.Plots.resize(gd);
-	};
-	socket.on('hit', function(channel_number){
-		console.log(channel_number);
-		channel_count[channel_number-1] += 1;
-
-		Plotly.redraw(gd);
-
-	});
-})();
+});
