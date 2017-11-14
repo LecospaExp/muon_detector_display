6ee4bd08687c82bd9b762a41aa6a3bc70d8e55de
TuringTW <kevin831130810620@gmail.com>
2017-11-14 16:27:50 +0800

    remove plotly and add pressure event

--- public/js/client.js
+++ public/js/client.js
@@ -1,53 +1,11 @@
-var socket = io('http://localhost:9487');
+var socket = io('http://140.112.104.83:9487');
 
 var channel_count = [0,0,0,0,0,0,0,0,0];
 
-
-(function(){
-	var d3 = Plotly.d3;
-
-	var WIDTH_IN_PERCENT_OF_PARENT = 60,
-	    HEIGHT_IN_PERCENT_OF_PARENT = 80;
-
-	var gd3 = d3.select('body')
-	    .append('div')
-	    .style({
-	        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
-	        'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
-
-	        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
-	        'margin-top': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
-	    });
-
-	var gd = gd3.node();
-
-	var data_set = {
-		type: 'bar',
-		x: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-	    y: channel_count,
-	    marker:{
-	    	color: '#C8A2C8',
-	    	line: {width: 2.5}
-	    } 
-	};
-
-	var data = [data_set];
-
-	var layout = {
-		title: 'Count-Channel',
-		font: {size: 16}
-	};
-
-	Plotly.plot(gd, data, layout, {displayModeBar: false});
-
-	window.onresize = function() {
-	    Plotly.Plots.resize(gd);
-	};
-	socket.on('hit', function(channel_number){
-		console.log(channel_number);
-		channel_count[channel_number-1] += 1;
-
-		Plotly.redraw(gd);
-
-	});
-})();
+socket.on('hit', function(channel_number){
+	console.log("hit: "+channel_number);
+	channel_count[channel_number-1] += 1;
+});
+socket.on('pressure', function(value){
+	console.log("pressure: "+value);
+});
