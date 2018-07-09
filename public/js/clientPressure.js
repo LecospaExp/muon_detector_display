f1edf8d469960b87f5633f8abb00a3c0c673b9a4
TuringTW <kevin831130810620@gmail.com>
2018-07-09 20:12:29 +0800

    add plot for pressure and count in clientPressure.js

--- public/js/clientPressure.js
+++ public/js/clientPressure.js
@@ -10,7 +10,6 @@ Math.myround = function(x, n){
 var ch2deg = [-80,-60,-40,-20,0,20,40,60,80];
 
 var strTime = Math.round(Date.now()/1000)
-var count = [0,0,0,0,0,0,0,0,0];
 var totalHit = 0;
 var ch_max = count.reduce(function(a,b){
     return Math.max(a,b);
@@ -20,17 +19,47 @@ var result; // Fitting function.
 var fit_count = [];
 var ctx = document.getElementById("pressure-time")
 
-var count_angle = new Chart(ctx, {
-	type: "line",
-    data: {},
-    options: {
-    	responsive: true
+var pressureTimePlot = new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets: [{
+      label: 'Count',
+      type: "line",
+      yAxisID: 'count',
+      data: [{0, 0}]
+    }, {
+      label: 'Pressure',
+      type: "line",
+      yAxisID: 'pressure',
+      data: [{0, 0}]
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        id: 'count',
+        type: 'linear',
+        position: 'left',
+      }, {
+        id: 'pressure',
+        type: 'linear',
+        position: 'right',
+      }]
     }
-    
+  }
 });
 socket.emit("getPressureTime");
 socket.on("pressureTimeData", function(res){
-    console.log(res);
+    console.log("pressureTime data get");
+    var ptData = [];
+    for (var i = res.length - 1; i >= 0; i--) {
+        ptData.push({t: new Date(res[i]._id*3600), myround(res[i].pressure, 4)})
+        ctData.push({t: new Date(res[i]._id*3600), res[i].count})
+    }
+    pressureTimePlot.data.datasets[0].data = ctData;
+    pressureTimePlot.data.datasets[1].data = ptData;
+    pressureTimePlot.update();
+
 })
 
 function timer(){
@@ -40,5 +69,5 @@ function timer(){
 }
 
 function resizeHist(){
-    count_angle.update();
+    pressureTimePlot.update();
 }
